0b6e27334a8ff2dd813b25ce038886b387518ff4
abhinav <abhinavisgreat18@gmail.com>
2024-07-20 12:04:29 -0700

    updated uerRoute

--- Router/userRoutes.js
+++ Router/userRoutes.js
@@ -31,7 +31,32 @@ router.post('/register', async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
+router.get('/settings/profileLogin',async(req,res)=>{
+    try {
+        const {username}=req.query
+        if (!username) {
+            return res.status(400).json({ message: 'Username is required' });
+        }
 
+
+        const checkUser = await Model.findOne({username:username})
+        if(!checkUser){
+            res.status(404).json({message:'User not found'})
+        }
+
+        else{
+            res.status(200).json({message:'User found Successfully', 
+            user:{
+                email:checkUser.email,
+                username:checkUser.username
+            }})
+        
+        }
+    } catch (error) {
+        console.error(error); // Log the error for debugging
+        return res.status(500).json({ message: 'Server error' });
+    }
+})
 router.post('/login',async(req,res)=>{
     try {
         const {email,password} = req.body
